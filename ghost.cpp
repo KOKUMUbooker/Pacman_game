76b50b2c8cf74a9ddfe48a142c051a9e6b317154
KOKUMUbooker <bookerochieng20@gmail.com>
2024-03-25 13:55:53 +0300

    Added comments on ghost logic

--- ghost.cpp
+++ ghost.cpp
@@ -33,9 +33,19 @@ void Ghost::update(std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map)
 	walls[2] = map_collision(0, 0, position.x - GHOST_SPEED, position.y, i_map);
 	walls[3] = map_collision(0, 0, position.x, GHOST_SPEED + position.y, i_map);
 
-    for(unsigned char a = 0; a <4; a++)
+    for(unsigned char a = 0; a < 4; a++)
     {
-        if(a == (direction + 2) % 4)
+        /*
+         Ghosts should never go backwards based on their current direction 
+         ie if direction == Right(0) : Left(2) should be inaccessible
+            if direction == Left(2) : Right(0) should be inaccessible
+            if direction == Up(1) : Down(3) should be inaccessible
+            if direction == Down(3) : Up(1) should be inaccessible
+            (direction + 2) % 4 refers to the inaccessible direction for all cases
+        */
+        unsigned char inaccessibleRoute = (direction + 2) % 4;
+
+        if(a == inaccessibleRoute)
         {
             continue;
         }
@@ -45,8 +55,10 @@ void Ghost::update(std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map)
         }
     }
 
+    // Check for places where there is more than a single entry point 
     if(available_ways > 1)
     {
+        std::cout<< "############ At junction #############" <<std::endl;
         unsigned char new_direction = rand() % 4;
 
         if(!walls[new_direction] && new_direction != (direction + 2) % 4)
@@ -54,9 +66,10 @@ void Ghost::update(std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH>& i_map)
             direction = new_direction;
         }
     }
+    // If there's still a wall in the current direction generate a a new one that isn't blocked by a wall
     else if(walls[direction])
     {
-         for(unsigned char a = 0; a <4; a++)
+         for(unsigned char a = 0; a < 4; a++)
          {
              if(!walls[a] && a != (direction + 2) % 4)
              {
